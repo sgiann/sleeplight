250f0bd6e53a77573601983fc3e7b84af4ee5cee
Stergios <stergios@Argetlam.Vroengard>
2017-02-25 12:24:08 +0000

    refined flashing classes. haven't tested yet.

--- flash_led_blink_thread_class.py
+++ flash_led_blink_thread_class.py
@@ -1,15 +1,22 @@
 import threading
 import time
 
+#param list
+#name : Give as name the colour of the LED to be handled.
+#pin_number - The pin number that the LED is hooked at.
+#dtime_mins - Give the desired time in minutes that you want the led to blink for.
+
 class LEDThreadStill(threading.Thread):
-    def __init__(self, name, pin_numbr, dtime_mins)
+    def __init__(self, name, pin_number, dtime_mins)
         threading.Thread._init__(self)
         self.name = name
         self.pin_number = pin_number
         self.dtime_mins = dtime_mins
 
+#the main funciton f this class
+#will blink the LED on for dtime mins.
     def run(self):
-        print "Thread start. Name:", self.name, " time:" ,datetime.datetime.now.time()
+        print "Thread start. Name:", self.name, " - blink. time:" ,datetime.datetime.now.time()
 
         #blink
         while true
--- flash_led_still_thread_class.py
+++ flash_led_still_thread_class.py
@@ -1,24 +1,22 @@
 import threading
 import time
 
-#params
-#name : Give as name the colur of the LED to be handled
-#dtime_mins: Give the desired time in minutes that you want the led to stay On
+#param list
+#name - Give as name the colour of the LED to be handled.
+#pin_number - The pin number that the LED is hooked at.
+#dtime_mins - Give the desired time in minutes that you want the led to stay On.
 
 class LEDThreadStill(threading.Thread):
-    def __init__(self, name, pin_numbr, dtime_mins)
+    def __init__(self, name, pin_number, dtime_mins)
         threading.Thread._init__(self)
         self.name = name
         self.pin_number = pin_number
         self.dtime_mins = dtime_mins
 
-#default function turns of the choosen led by seting
-#to low the pin number that takes as argument
-    def default(pin_number):
-        GPIO.output(self.pin_number, GPIO.LOW)
-
+#the main funciton f this class
+#will turn the LED on for dtime mins.
     def run(self):
-        print "Thread start. Name:", self.name, " time:" ,datetime.datetime.now.time()
+        print "Thread start. Name:", self.name, " - still. time:" ,datetime.datetime.now.time()
 
         #turn LED off in case it is on.
         self.default(self.pin_number)
@@ -30,3 +28,8 @@ class LEDThreadStill(threading.Thread):
         self.default(self.pin_number)
 
         print "Thread end. Name:", self.name, " time:" ,datetime.datetime.now.time()
+
+#default function turns of the choosen led by setting
+#to low the pin number that takes as argument
+    def default(pin_number):
+        GPIO.output(self.pin_number, GPIO.LOW)
